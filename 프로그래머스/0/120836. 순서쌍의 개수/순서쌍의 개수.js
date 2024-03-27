f88c2c1b9f9a71bcb9d4e11a665887e6d636582c
BAMBI <116716953+Bambiru@users.noreply.github.com>
2024-03-27 09:56:32 +0900

    [level 0] Title: 순서쌍의 개수, Time: 6.04 ms, Memory: 36.4 MB -BaekjoonHub

--- 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.js
+++ 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.js
@@ -1,12 +1,8 @@
 function solution(n) {
-    let answer = [];
-    // 1부터 n까지 돌면서  1*n은 n, 2*n/2는 n 4*n/4
-    //  Number.isInteger() 
-    for(let i=1; i<=n ; i+=1) {
-        let number = n/i;
-        if(Number.isInteger(number)) {
-        answer.push(number);
-        }   
+    let answer = 0;
+    
+    for(let i=0; i <= n; i+=1) {
+        if(n%i==0) answer++
     }
-    return answer.length;
+ return answer;   
 }
