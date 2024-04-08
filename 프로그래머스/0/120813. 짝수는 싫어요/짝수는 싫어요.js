56bfea28b1720251601bf48db76c3933602d6d57
BAMBI <116716953+Bambiru@users.noreply.github.com>
2024-04-08 22:33:45 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.js
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.js
@@ -1,8 +1,12 @@
 function solution(n) {
-    let answer = [];
-    // n 이하의 홀수를 찾고, 그걸 배열로 리턴
-    for(let i=1; i<=n; i+=2) {
-        answer.push(i)
+    var answer = [];
+    
+    // n 이하의 홀수
+    
+    for(let i=1; i<=n ; i++){
+        
+        if(i%2 === 1) answer.push(i);
+        
     }
     
     return answer;
