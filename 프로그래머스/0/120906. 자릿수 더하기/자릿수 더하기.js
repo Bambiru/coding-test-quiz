14be71f8c8d1e880073d026e9cdf9846c305c5d6
BAMBI <116716953+Bambiru@users.noreply.github.com>
2024-04-08 22:48:09 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.js
+++ 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.js
@@ -1,10 +1,14 @@
 function solution(n) {
-    const str = String(n);
-    const mapfn = (arg) => Number(arg);
-    let newArr = Array.from(str, mapfn);
     
-    return newArr.reduce((acc,curr)=>acc+curr,0)
+    let str = n+'';
+    let strArr = str.split('');
+    let numArr = [];
     
+    for(let str of strArr) {
+        
+        numArr.push(+str);
+    }
     
-    
+    return numArr.reduce((a,b)=>a+b,0);
+
 }
