7b0aea15d30f19a45b53b299f7a4e39483757dc0
BAMBI <116716953+Bambiru@users.noreply.github.com>
2024-05-08 20:46:14 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.js
+++ 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.js
@@ -1,6 +1,6 @@
 function solution(array) {
-    const sorted = [...array].sort((a,b)=>b-a);
-    const b = array.indexOf(sorted[0]);
+    const maxNum = Math.max(...array);
+    const b = array.indexOf(maxNum);
     
-    return [sorted[0], b];
+    return [maxNum, b];
 }
