c43435619a87d9e86cb33d43dd3bd3d8556a1a5c
BAMBI <116716953+Bambiru@users.noreply.github.com>
2024-05-08 21:12:19 +0900

    [level 0] Title: 암호 해독, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120892. 암호 해독/암호 해독.js
+++ 프로그래머스/0/120892. 암호 해독/암호 해독.js
@@ -2,8 +2,8 @@ function solution(cipher, code) {
     let result='';
     // 길이를 돌면서, code번째에 해당하는 값을 가져온다.
         
-        for(let i=1;i<=cipher.length/code;i++) {
-            if(i / code) { result += cipher[(code*i)-1] }         
+        for(let i=code - 1 ; i < cipher.length ; i += code) {
+            result += cipher[i]        
         }
     
     
